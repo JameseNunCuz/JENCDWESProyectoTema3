c01d5f1fe373e59df16c6cac62b1dbc622d6bc10
JameseNunCuz <jamese.nuncuz.1@educa.jcyl.es>
2025-10-20 13:02:18 +0200

    Ejercicio 20 empezado, no se hacerlo, por ahora lo dejo, incluida libreria validación

---
+++ codigoPHP/ejercicio20.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 20</title>
+</head>
+
+<body>
+    <?php /** 
+      *@author James Edward
+      *@since 20/10/2025
+      *@version 20/10/2025
+      */
+
+    require("../core/231018libreriaValidacion.php");
+
+    ?>
+</body>
+
+</html>
